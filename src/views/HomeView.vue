48827c89232b0a173a2562069b54a6dfbc84f202
AliHCS <aligusali@gmail.com>
2024-04-18 21:40:06 -0600

    Componentes y props

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,9 +8,30 @@
         </p>
       </v-col>
     </v-row>
+    <GridCardsComponent :cardData="cardData" />
   </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GridCardsComponent from "@/components/GridCardsComponent.vue"; // Ajusta la ruta según la estructura de tu proyecto
+
+const cardData = [
+  {
+    title: "Pre-fab homes",
+    src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+    flex: 12,
+  },
+  {
+    title: "Favorite road trips",
+    src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+    flex: 6,
+  },
+  {
+    title: "Best airlines",
+    src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+    flex: 6,
+  },
+];
+</script>
 
 <style scoped></style>
